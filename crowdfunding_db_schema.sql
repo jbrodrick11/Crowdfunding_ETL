1d4e0c1f680c0f9599e5d5127e7903d01a943f1e
jbrodrick11 <116480388+jbrodrick11@users.noreply.github.com>
2023-02-20 23:58:55 +1100

    Push added sql file and ERD

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,38 @@
+/*Create Category table*/
+CREATE TABLE category (
+	category_id VARCHAR(30) NOT NULL PRIMARY KEY,
+	category VARCHAR(30) NOT NULL
+	);
+
+/*Create Subcategory table*/
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(30) NOT NULL PRIMARY KEY,
+	subcategory VARCHAR(30) NOT NULL
+	);
+	
+/*Create Contacts table*/
+CREATE TABLE contacts (
+	contact_id INTEGER NOT NULL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL
+	);
+	
+/*Create Campaign table*/
+CREATE TABLE campaign (
+	cf_id INTEGER NOT NULL,
+	contact_id INTEGER NOT NULL REFERENCES contacts(contact_id),
+	company_name VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR(30) NOT NULL,
+	backers_count INTEGER NOT NULL,
+	country VARCHAR(30) NOT NULL,
+	currency VARCHAR(30) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(30) NOT NULL REFERENCES category(category_id),
+	subcategory_id VARCHAR(30) NOT NULL REFERENCES subcategory(subcategory_id)
+	);
+
